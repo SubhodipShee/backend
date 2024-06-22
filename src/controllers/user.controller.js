d8229132c7381de4086ee1fcee3547eec436499f
Subhodp <subhodipshee17@gmail.com>
2024-06-22 11:20:28 +0530

    register controller bugs fixing

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -21,7 +21,7 @@ const registerUser = asyncHandler( async (req, res) => {
     // return response
 
     const {fullName, email, username, password } = req.body
-    console.log("email: ", email);
+    // console.log("email: ", email);
 
     /* if(fullName ==""){
         throw new ApiError(400, "fullname required")
@@ -31,6 +31,7 @@ const registerUser = asyncHandler( async (req, res) => {
     ) {
         throw new ApiError(400, "All fields are required")
     }
+
     const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
@@ -42,10 +43,12 @@ const registerUser = asyncHandler( async (req, res) => {
     const avatarLocalPath = req.files?.avatar[0]?.path;
     //const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
     
-    
-        const coverImageLocalPath = req.files?.coverImage[0]?.path;
-    
+       
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
     }
@@ -56,6 +59,7 @@ const registerUser = asyncHandler( async (req, res) => {
     if (!avatar) {
         throw new ApiError(400, "Avatar file is required")
     }
+
     const user = await User.create({
         fullName,
         avatar: avatar.url,
@@ -64,12 +68,15 @@ const registerUser = asyncHandler( async (req, res) => {
         password,
         username: username.toLowerCase()
     })
+
     const createdUser = await User.findById(user._id).select(
         "-password -refreshToken"
     )
+
     if (!createdUser) {
         throw new ApiError(500, "Something went wrong while registering the user")
     }
+
     return res.status(201).json(
         new ApiResponse(200, createdUser, "User registered Successfully")
     )
@@ -77,10 +84,6 @@ const registerUser = asyncHandler( async (req, res) => {
 
 
 } )
-
-
-
-
 export {
     registerUser
 }
